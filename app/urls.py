b4dc29dae8d9215a36284f26281d636c5841fca2
1montandon <emontandon11@gmail.com>
2024-11-21 14:59:49 -0300

    Criação da API para Autor

--- app/urls.py
+++ app/urls.py
@@ -11,12 +11,14 @@ from rest_framework.routers import DefaultRouter
 from core.views import UserViewSet
 from core.views import CategoriaViewSet
 from core.views import EditoraViewSet
+from core.views import AutorViewSet
 
 router = DefaultRouter()
 
 router.register(r"usuarios", UserViewSet, basename="usuarios")
 router.register(r"categorias", CategoriaViewSet)	
 router.register(r"editoras", EditoraViewSet)
+router.register(r"autores", AutorViewSet)
 
 
 urlpatterns = [
