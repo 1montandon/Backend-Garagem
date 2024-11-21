d485a61c3e8709a9fe06e0c23e57b8bfc4b3e8e9
1montandon <emontandon11@gmail.com>
2024-11-20 21:21:35 -0300

    Criação da API para Editora

--- app/urls.py
+++ app/urls.py
@@ -10,11 +10,14 @@ from rest_framework.routers import DefaultRouter
 
 from core.views import UserViewSet
 from core.views import CategoriaViewSet
+from core.views import EditoraViewSet
 
 router = DefaultRouter()
 
 router.register(r"usuarios", UserViewSet, basename="usuarios")
 router.register(r"categorias", CategoriaViewSet)	
+router.register(r"editoras", EditoraViewSet)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
