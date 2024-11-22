7854e865fa75123042319fb2120a750bba43a335
1montandon <emontandon11@gmail.com>
2024-11-22 09:49:15 -0300

    Criação da entidade para Livro, Remove Passage authentication

--- app/urls.py
+++ app/urls.py
@@ -8,10 +8,7 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 
-from core.views import UserViewSet
-from core.views import CategoriaViewSet
-from core.views import EditoraViewSet
-from core.views import AutorViewSet
+from core.views import UserViewSet, CategoriaViewSet, EditoraViewSet, AutorViewSet, LivroViewSet
 
 router = DefaultRouter()
 
@@ -19,6 +16,7 @@ router.register(r"usuarios", UserViewSet, basename="usuarios")
 router.register(r"categorias", CategoriaViewSet)	
 router.register(r"editoras", EditoraViewSet)
 router.register(r"autores", AutorViewSet)
+router.register(r"livros", LivroViewSet)
 
 
 urlpatterns = [
