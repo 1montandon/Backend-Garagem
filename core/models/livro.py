0bf91e09a21c4c82d92e4fa27946e8701b14c6fa
1montandon <emontandon11@gmail.com>
2024-11-22 13:21:16 -0300

    Inclusão do relacionamento n para n no modelo do Livro

--- core/models/livro.py
+++ core/models/livro.py
@@ -11,6 +11,7 @@ class Livro(models.Model):
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0, null=True, blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, null=True, blank=True, related_name="livros")
     editora = models.ForeignKey(Editora, on_delete=models.PROTECT, null=True, blank=True, related_name="livros")
+    autores = models.ManyToManyField(Autor, related_name="livros", blank=True)
 
     def __str__(self):
         return f"({self.id}) {self.titulo} ({self.quantidade})"
